52075111cdc7317f7d146dbead65d4aed878bd77
Jingo88 <jcory.ng@gmail.com>
2015-01-23 14:47:55 -0500

    added text file with api key

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var app = express();
 var request = require('request');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
 app.use(express.static('public'));
 
+var apikey = fs.readFileSync('api_key.txt', 'utf8');
 
 app.get('/', function(req, res){
 	
