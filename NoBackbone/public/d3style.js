d0d97ae2ed417bd379c6ac916656e8a2ad92a543
Jingo88 <jcory.ng@gmail.com>
2015-02-15 20:38:09 -0500

    still working on d3

--- NoBackbone/public/d3style.js
+++ NoBackbone/public/d3style.js
@@ -1,61 +1,64 @@
-// function donationCircles(data){
-// 	var svg = d3.select('svg');
+function donationCircles(data){
+	console.log(data);
+	console.log(data.Total_Amount)
+	var svg = d3.select('svg');
 
-// 	var circles = svg.selectAll('circle')
-// 						.data(data.Total_Amount)
-// 						.enter()
-// 						.append('circle');
+	var circles = svg.selectAll('circle')
+						.data(data)
+						.enter()
+						.append('circle');
 
-// 	var circles = svg.selectAll('circle')
-// 						.data(data.Total_Amount)
-// 							.attr('r', function(d){return d*10 + "px"})
-// 							.attr('cx', function(){return Math.random()*100 + "%"})
-// 							.attr('cy', function(){return Math.random()*100 + "%"})
-// 							.style('fill', '#cc5a62');
+	var circlesAttributes = circles
+						// .data(data.Total_Amount)
+							.data(data)
+							.attr('r', 100 + "px")
+							.attr('cx', function(){return Math.random()*100 + "%"})
+							.attr('cy', function(){return Math.random()*100 + "%"})
+							.style('fill', '#cc5a62');
 
 	
-// 	svg.selectAll('circle')
-// 		.data(data.Total_Amount)
-// 		.exit()
-// 		.remove();														
-// }
+	// svg.selectAll('circle')
+	// 	.data(data)
+	// 	.exit()
+	// 	.remove();														
+}
 
 //donationArr is a array of hashes with the name of the organization and the donation amount
 
-function donationCircles(data){
-	var svg = d3.select('svg');
+// function donationCircles(data){
+// 	var svg = d3.select('svg');
 
-	var nodes = svg.append("g")
-           .attr("class", "nodes")
-           .selectAll("circle")
-           .data(data)
-           .enter()
-           // Add one g element for each data node here.
-           .append("g");
-           // Position the g element like the circle element used to be.
+// 	var nodes = svg.append("g")
+//            .attr("class", "nodes")
+//            .selectAll("circle")
+//            .data(data)
+//            .enter()
+//            // Add one g element for each data node here.
+//            .append("g");
+//            // Position the g element like the circle element used to be.
 				
 				
 
-           // Add a circle element to the previously added g element.
-	nodes.append("circle")
-		.attr("class", "node")
-		.style("fill", "#cc5a62")
-		.attr('r', function(d){return d*10 + "px"})
-		.attr('cx', function(){return Math.random()*100 + "%"})
-		.attr('cy', function(){return Math.random()*100 + "%"});
+//            // Add a circle element to the previously added g element.
+// 	nodes.append("circle")
+// 		.attr("class", "node")
+// 		.style("fill", "#cc5a62")
+// 		.attr('r', function(d){return d*10 + "px"})
+// 		.attr('cx', function(){return Math.random()*100 + "%"})
+// 		.attr('cy', function(){return Math.random()*100 + "%"});
 
-// Add a text element to the previously added g element.
-	nodes.append("text")
-		.attr("text-anchor", "middle")
-		.text(function(d) {
-		return d.Organization_Name;
-		});
+// // Add a text element to the previously added g element.
+// 	nodes.append("text")
+// 		.attr("text-anchor", "middle")
+// 		.text(function(d) {
+// 		return d.Organization_Name;
+// 		});
 
-	svg.selectAll('circle')
-		.data(data)
-		.exit()
-		.remove();
-};
+// 	svg.selectAll('circle')
+// 		.data(data)
+// 		.exit()
+// 		.remove();
+// };
 
 
 
