132db40fcad2cdc76eb617de2adf08792a2e4825
DavideDaniel <davidedaniel@gmail.com>
2015-04-19 23:34:52 -0700

    added bills to schema and ability to add to api

--- models/legislator.js
+++ models/legislator.js
@@ -1,48 +1,48 @@
 var mongoose = require('mongoose');
 var LegislatorSchema = new mongoose.Schema({
-	bioguide_id: String,
-	crp_id: String,
-	first_name: String,
-	last_name: String,
-	state_name: String,
-	state: String,
-	party: String,
-	chamber: String,
-	gender: String,
-	twitter_id: String,
-	term_start: Date,
-	term_end: Date,
-	website: String,
-	in_office: Boolean,
-	bills: {
-		enacted_true: Number,
-		enacted_false: Number,
-		active_count: Number,
-		vetoed_count: Number,
-		topics: Array
-	},
-	industries: [ {
-		industry_name: String,
-		industry_code: String,
-		money_from_indivs: Number,
-		money_from_pacs: Number 
-	} ],
-	sectors: [ {
-		sector_name: String,
-		sectorid: String,
-		money_from_indivs: Number,
-		money_from_pacs: Number 
-	} ],
-	moneys: [ {
-		total_reciepts: Number,
+    bioguide_id: String,
+    crp_id: String,
+    first_name: String,
+    last_name: String,
+    state_name: String,
+    state: String,
+    party: String,
+    chamber: String,
+    gender: String,
+    twitter_id: String,
+    term_start: Date,
+    term_end: Date,
+    website: String,
+    in_office: Boolean,
+    bills: [{
+        bill_id: String,
+        official_title: String,
+        introduced_on: Date,
+        active: Boolean,
+        enacted: Boolean,
+        vetoed: Boolean,
+    }],
+    industries: [{
+        industry_name: String,
+        industry_code: String,
+        money_from_indivs: Number,
+        money_from_pacs: Number
+    }],
+    sectors: [{
+        sector_name: String,
+        sectorid: String,
+        money_from_indivs: Number,
+        money_from_pacs: Number
+    }],
+    moneys: [{
+        total_reciepts: Number,
         total_spent: Number,
         cash_on_hand: Number,
         debt: Number
-	} ],
-	updated_at: {
-		type: Date,
-		default: Date.now
-	}
+    }],
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
 })
-
 module.exports = mongoose.model('Legislator', LegislatorSchema)
--- routes/api.js
+++ routes/api.js
@@ -19,15 +19,11 @@ router.get('/', function(req, res, next) {
     console.log('hit /api');
     res.json('nothing to see here');
 })
-
-
-
 // router.get('/addlegislator/:last_name', function(req, res, next) {
 //     console.log('hit /api');
 //     var name = toTitleCase(req.params.last_name);
 //     res.redirect('/addlegislator/'+name)
 // })
-
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -40,32 +36,31 @@ router.get('/addlegislator/:last_name', function(req, res, next) {
     var name = toTitleCase(req.params.last_name);
     var sunurl = "https://congress.api.sunlightfoundation.com/legislators?fields=&apikey=" + sunKey + "&last_name=" + name;
     request(sunurl, function(error, response, data) {
-    	var senatorObj = JSON.parse(data);
-            var results = senatorObj.results;
-         if (!error && response.statusCode == 200) {
+        var senatorObj = JSON.parse(data);
+        var results = senatorObj.results;
+        if (!error && response.statusCode == 200) {
             var legislator = new Legislator({
-            bioguide_id: results[0].bioguide_id,
-            crp_id: results[0].crp_id,
-            firstName: results[0].first_name,
-            lastName: results[0].last_name,
-            state_name: results[0].state_name,
-            state: results[0].state,
-            party: results[0].party,
-            chamber: results[0].chamber,
-            gender: results[0].gender,
-            term_start: results[0].term_start,
-            term_end: results[0].term_end,
-            website: results[0].website,
-            in_office: results[0].in_office,
-            twitter: results[0].twitter_id
-            
-        })
+                bioguide_id: results[0].bioguide_id,
+                crp_id: results[0].crp_id,
+                firstName: results[0].first_name,
+                lastName: results[0].last_name,
+                state_name: results[0].state_name,
+                state: results[0].state,
+                party: results[0].party,
+                chamber: results[0].chamber,
+                gender: results[0].gender,
+                term_start: results[0].term_start,
+                term_end: results[0].term_end,
+                website: results[0].website,
+                in_office: results[0].in_office,
+                twitter: results[0].twitter_id
+            })
             console.log(legislator);
             res.json(legislator);
-            legislator.save(function (err) {
-  if (err) return console.log(err);
-  // saved!
-})
+            legislator.save(function(err) {
+                if (err) return console.log(err);
+                // saved!
+            })
             // if (results.length === 1) {
             //     Legislator.create(results, function(err, post))
             // }
@@ -79,7 +74,109 @@ router.get('/index', function(req, res, next) {
         res.json(legislators);
     });
 });
+router.route('/legislators/:id').get(function(req, res, next) {
+    Legislator.findById(req.params.id, function(err, result) {
+        if (err) return next(err);
+        res.json(result);
+    });
+}).put(function(req, res, next) {
+    Legislator.findById(req.params.id, function(err, legislator) {
+        if (err) return next(err);
+        console.log(legislator);
+        var bio = legislator.bioguide_id;
+        console.log(bio);
+        var billsurl = "https://congress.api.sunlightfoundation.com/bills?&apikey=" + sunKey + "&sponsor_id=" + bio;
+        request(billsurl, function(error, response, data) {
+            if (!error && response.statusCode == 200) {
+                var billObj = JSON.parse(data);
+                var results = billObj.results;
+                for (var i = 0; i < results.length; i++) {
+                    Legislator.update({
+                        _id: legislator.id
+                    }, {
+                        $push: {
+                            "bills": {
+                                bill_id: results[i].bill_id,
+                                official_title: results[i].official_title,
+                                introduced_on: results[i].introduced_on,
+                                active: results[i].history.active,
+                                enacted: results[i].history.enacted,
+                                vetoed: results[i].history.active
+                            }
+                        }
+                    }),
+                    function(err) {
+                        if (err) return console.log(err);
+                        // saved!
+                        console.log("successful");
+                    }
+                }
+                res.redirect('/api/index')
+                // res.send(data)
+            }
+        })
+    })
+})
 // UPDATE
+router.get('/addbills/:id', function(req, res) {
+    Legislator.findById(req.params.id, function(err, legislator) {
+        if (err) return next(err);
+        console.log(legislator);
+        var bio = legislator.bioguide_id;
+        console.log(bio);
+        var billsurl = "https://congress.api.sunlightfoundation.com/bills?&apikey=" + sunKey + "&sponsor_id=" + bio;
+        request(billsurl, function(error, response, data) {
+            if (!error && response.statusCode == 200) {
+                var billObj = JSON.parse(data);
+                var results = billObj.results;
+                var bills = [];
+                for (var i = 0; i < results.length; i++) {
+                    var bill = {
+                        bill_id: results[i].bill_id,
+                        official_title: results[i].official_title,
+                        introduced_on: results[i].introduced_on,
+                        active: results[i].history.active,
+                        enacted: results[i].history.enacted,
+                        vetoed: results[i].history.active
+                    }
+                    bills.push(bill)
+}
+console.log(bills);
+                    Legislator.update({
+                        _id: legislator._id
+                    }, {
+                        $set: {
+                            "bills": bills
+                        }
+                    }, function(err) {
+                        if (err) return res.send("contact addMsg error: " + err);
+
+
+                    })
+                    // res.send(data)
+                
+                res.redirect('/api/index')
+            }
+        })
+    })
+    // console.log(query);
+    // res.send(query)
+    // var legs = Legislator.findByIdAndUpdate()
+    // var bio = req.params.bioID;
+    // var billsurl = "https://congress.api.sunlightfoundation.com/bills?&apikey=" + sunKey + "&sponsor_id=" + bio;
+    // request(billsurl, function(error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         // res.send(body)
+    //     }
+    // })
+    // res.send(update_id)
+});
 // DESTROY
+router.delete('/legislators/:id', function(req, res, next) {
+    Legislator.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
 // probably won't have a destroy
 module.exports = router;
